c99ce5ae95168cd93cadd928a3a1df13b980d56c
andr.kuznetsov <andr.kuznetsov@corp.vk.com>
2021-07-27 17:59:51 +0300

    change-package-structure

---
+++ pim/transformers.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def generate_pseudo_observations(target: np.ndarray):
+    if isinstance(target, np.ndarray):
+        mask = target[:, None] > target
+        return mask[~np.eye(target.size, dtype=bool)]
+    else:
+        raise ValueError('target should be np.ndarray type')
+
+
+def generate_pseudo_observations_with_ties(target: np.ndarray):
+    if isinstance(target, np.ndarray):
+        mask = target[:, None] == target
+        mask = mask[~np.eye(target.size, dtype=bool)]
+        print(mask)
+    else:
+        raise ValueError('target should be np.ndarray type')
+
+
+def generate_predictors(predictors: np.ndarray):
+    if isinstance(predictors, np.ndarray):
+        predictors = predictors if len(predictors.shape) > 1 else predictors.reshape(predictors.shape[0], 1)
+        features = []
+        for column_idx in range(predictors.shape[1]):
+            predictor = predictors[:, column_idx]
+            mask = predictor[:, None] - predictor
+            features.append(mask[~np.eye(predictor.size, dtype=bool)])
+        return np.vstack(features).T
+    else:
+        raise ValueError('predictors should be np.ndarray type')
